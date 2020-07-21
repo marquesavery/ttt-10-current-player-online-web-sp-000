40c9c2f528ebe24fc501a147c3db57f32e6dbda8
Marques Avery <averymarques4@gmail.com>
2020-07-21 23:05:38 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
 def turn_count(board)
   counter = 0
-  board.each do |turn|
-    if "#{turn}" == "X" || "O"
-      counter += 1
+  if "#{turn}" == "X" || "O"
+    board.each do |turn|
+    counter += 1
     end
   end
   return counter
